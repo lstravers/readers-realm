cc40a665cfe137dac0bf872643c07c8a32fbdf13
lstravers <stravers222@gmail.com>
2018-08-27 23:41:13 -0400

    modified files.

--- book-application/app/controllers/books_controller.rb
+++ book-application/app/controllers/books_controller.rb
@@ -1,19 +1,34 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
 
   def new
   end
 
-  def index
-  end
-
-
-  # def create
-  # end
-
   # def edit
   # end
+
+  def create
+    @book = Book.new(book_params)
+ 
+    @book.save
+    redirect_to @book
+  end
+   
+  private
+    def book_params
+      params.require(:book).permit(:title, :author, :description, :url)
+  end
+
+  # def update
+  # end
+
+  # def destroy
+  # end
   
-
-
-
 end
