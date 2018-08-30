d6748d23d36d74a53199538e4648f51b74c9dec1
lstravers <stravers222@gmail.com>
2018-08-30 09:15:30 -0400

    generated db migration.

--- book-application/db/schema.rb
+++ book-application/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_223501) do
+ActiveRecord::Schema.define(version: 2018_08_30_131313) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(version: 2018_08_29_223501) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email"
     t.string "password_digest"
+    t.string "email"
   end
 
 end
