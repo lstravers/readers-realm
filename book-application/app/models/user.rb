a02161338cd1bf2a9e5f4ab0dc856526174b26f1
lstravers <stravers222@gmail.com>
2018-08-30 08:32:31 -0400

    created sessions controller.

--- book-application/app/models/user.rb
+++ book-application/app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_many :books
 
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+end
+
   # validates :name, presence: true,
   #                   length: { minimum: 3 }
 end
--- book-application/db/schema.rb
+++ book-application/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_034419) do
+ActiveRecord::Schema.define(version: 2018_08_29_223501) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2018_08_29_034419) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
